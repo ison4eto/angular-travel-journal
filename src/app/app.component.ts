be26b9c8bf692a1eae3614ff68349117ec1df64b
iskra <ison4eto635@gmail.com>
2018-08-23 07:46:11 +0300

    Angular project without documentation and admin roles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDvomM7opgK-nH48TuxcA2U8g7r7S68FGI",
+      authDomain: "travel-journal-project.firebaseapp.com"
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './core/interceptors/token.interceptor';
+
+import { AppRoutingModule } from './app.routing';
+import { StoriesRoutingModule } from './components/stories/stories.routing';
+import { SharedModule } from './components/shared/shared.module';
+import { AuthenticationModule } from './components/authentication/authentication.module';
+import { StoriesModule } from './components/stories/stories.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    SharedModule,
+    AuthenticationModule,
+    StoriesModule,
+    StoriesRoutingModule,
+    AppRoutingModule
+
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
